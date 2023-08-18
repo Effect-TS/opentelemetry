3939b049a8753ac89b1e2b47b8378b2a415fc452
Tim <hello@timsmart.co>
2023-08-18 16:20:51 +1200

    fix test

--- test/Metrics.test.ts
+++ test/Metrics.test.ts
@@ -1,4 +1,5 @@
 import * as Effect from "@effect/io/Effect"
+import { disableRuntimeMetrics } from "@effect/io/Fiber/Runtime/Flags"
 import * as Metric from "@effect/io/Metric"
 import * as internal from "@effect/opentelemetry/internal/metrics"
 import * as Metrics from "@effect/opentelemetry/Metrics"
@@ -7,7 +8,7 @@ import { Resource } from "@opentelemetry/resources"
 
 describe("Metrics", () => {
   it("gauge", () =>
-    Effect.runPromise(Effect.gen(function*(_) {
+    Effect.gen(function*(_) {
       const runtime = yield* _(Effect.runtime<never>())
       const producer = new internal.MetricProducerImpl(
         runtime,
@@ -20,51 +21,39 @@ describe("Metrics", () => {
         Metrics.integerLabel,
         Metrics.unitLabel("requests")
       ])
+
       yield* _(Metric.set(gauge, 10), Effect.tagMetrics("key", "value"))
 
       const results = yield* _(Effect.promise(() => producer.collect()))
-      console.log(JSON.stringify(results, null, 2))
       const object = JSON.parse(JSON.stringify(results))
-      assert.deepEqual(object, {
-        "resourceMetrics": {
-          "resource": {
-            "service.name": "test",
-            "telemetry.sdk.name": "@effect/opentelemetry/Metrics",
-            "telemetry.sdk.language": "nodejs",
-            "service.version": "1.0.0"
-          },
-          "scopeMetrics": [
-            {
-              "scope": {
-                "name": "@effect/opentelemetry/Metrics"
-              },
-              "metrics": [
-                {
-                  "dataPointType": 2,
-                  "descriptor": {
-                    "name": "rps",
-                    "description": "",
-                    "unit": "requests",
-                    "type": "OBSERVABLE_GAUGE",
-                    "valueType": ValueType.INT
-                  },
-                  "aggregationTemporality": 1,
-                  "dataPoints": [
-                    {
-                      "startTime": results.resourceMetrics.scopeMetrics[0].metrics[0].dataPoints[0].startTime,
-                      "endTime": results.resourceMetrics.scopeMetrics[0].metrics[0].dataPoints[0].endTime,
-                      "attributes": {
-                        "key": "value"
-                      },
-                      "value": 10
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-        "errors": []
+      assert.deepEqual(object.resourceMetrics.resource._attributes, {
+        "name": "test",
+        "version": "1.0.0"
       })
-    })))
+      assert.equal(object.resourceMetrics.scopeMetrics.length, 1)
+      assert.deepEqual(object.resourceMetrics.scopeMetrics[0].metrics.find((_: any) => _.descriptor.name === "rps"), {
+        "dataPointType": 2,
+        "descriptor": {
+          "name": "rps",
+          "description": "",
+          "unit": "requests",
+          "type": "OBSERVABLE_GAUGE",
+          "valueType": ValueType.INT
+        },
+        "aggregationTemporality": 1,
+        "dataPoints": [
+          {
+            "startTime": results.resourceMetrics.scopeMetrics[0].metrics[0].dataPoints[0].startTime,
+            "endTime": results.resourceMetrics.scopeMetrics[0].metrics[0].dataPoints[0].endTime,
+            "attributes": {
+              "key": "value"
+            },
+            "value": 10
+          }
+        ]
+      })
+    }).pipe(
+      Effect.provideLayer(disableRuntimeMetrics),
+      Effect.runPromise
+    ))
 })
--- test/utils/extend.ts
+++ test/utils/extend.ts
@@ -1,5 +1,6 @@
 import { pipe } from "@effect/data/Function"
 import * as Effect from "@effect/io/Effect"
+import { disableRuntimeMetrics } from "@effect/io/Fiber/Runtime/Flags"
 import * as V from "vitest"
 
 export type API = V.TestAPI<{}>
@@ -17,6 +18,7 @@ export const effect = (() => {
       () =>
         pipe(
           Effect.suspend(self),
+          Effect.provideSomeLayer(disableRuntimeMetrics),
           Effect.runPromise
         ),
       timeout
@@ -33,6 +35,7 @@ export const effect = (() => {
         () =>
           pipe(
             Effect.suspend(self),
+            Effect.provideSomeLayer(disableRuntimeMetrics),
             Effect.runPromise
           ),
         timeout
@@ -48,6 +51,7 @@ export const effect = (() => {
         () =>
           pipe(
             Effect.suspend(self),
+            Effect.provideSomeLayer(disableRuntimeMetrics),
             Effect.runPromise
           ),
         timeout
