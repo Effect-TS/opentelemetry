4711d6c10544fe05842b696d2195642eb496d1b3
Tim <hello@timsmart.co>
2023-10-04 12:01:35 +1300

    add preconstruct

---
+++ .changeset/rare-dolphins-hug.md
@@ -0,0 +1,5 @@
+---
+"@effect/opentelemetry": minor
+---
+
+switch to preconstruct for builds
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,11 @@
 /// <reference types="vitest" />
 
-import babel from "@vitejs/plugin-react"
-import path from "path"
+import * as path from "node:path"
 import { defineConfig } from "vite"
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const babelConfig = require("./.babel.mjs.json")
-
 export default defineConfig({
-  plugins: [babel({ babel: babelConfig })],
   test: {
-    include: ["./test/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    exclude: ["./test/utils/**/*.ts", "./test/**/*.init.ts"],
+    include: ["./test/**/*.test.ts"],
     globals: true
   },
   resolve: {
